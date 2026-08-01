9ebb9acba2660df642fe5b5d8488e0eafe04ac63
fatima1886 <fatima782323@gmail.com>
2026-08-01 17:47:16 +0500

    Hamburger menu added

---
+++ src/hamburger.js
@@ -0,0 +1,13 @@
+// export function showSideMenu() {
+//     const lineone = document.querySelector(".one");
+//      const linetwo = document.querySelector(".two");
+//       const linethree = document.querySelector(".three");
+//       const sideMenu = document.querySelector(".sideMenu");
+//       if(hambugerBtn.classlist.acive){
+//         lineone.style.transform = "rotate(45deg)";
+//      linetwo.style.display = "none";
+//      linethree.style.transform = "rotate(-45deg)";
+//      sideMenu.style.transform = "translate(0)";
+//       }
+     
+// }
