26f22a7c3b182c6fb79cf41d296d3397e9fe55fb
fatima1886 <fatima782323@gmail.com>
2026-08-02 09:39:43 +0500

    alert added on save btn

--- src/notes.js
+++ src/notes.js
@@ -1,31 +1,4 @@
-// const title = document.getElementById("note-title");
-// const body = document.getElementById("note-body");
-// const showTitle = document.querySelector(".title");
-// const showBody = document.querySelector(".body");
-// const saveButton = document.querySelector(".Savebtn");
-// const listItems = document.querySelector(".listItem");
 
-// function saveNote() {
-//     const noteCreated = {
-//         title: title.value,
-//         body: body.value
-//     };
-//     // Save JSON string to localStorage
-//     localStorage.setItem("noteCreated", JSON.stringify(noteCreated));
-//     // Wipe inputs dry clean
-//     title.value = "";
-//     body.value = "";
-// }
-// import { ForListItem } from "listItem.js";
-
-// saveButton.addEventListener("click", () => {
-//     saveNote();
-//     const checkSavedData = localStorage.getItem("noteCreated");
-//     if (checkSavedData) {
-//         const parsedData = JSON.parse(checkSavedData);
-//         ForListItem();
-//     }
-// });
 
 // 1. Imports must always be at the top of the file
 import { ForListItem } from "./listItem.js";
@@ -35,25 +8,30 @@ const title = document.getElementById("note-title");
 const body = document.getElementById("note-body");
 const saveButton = document.querySelector(".Savebtn");
 const listItems = document.querySelector(".listItem");
+const alertPara = document.querySelector(".alertPara");
 
 // 3. Logic Functions
 function saveNote() {
+    if (title.value === "" || body.value === "") {
+        alertPara.classList.remove("hidden");
+return;
+    }
+else{
+    alertPara.classList.add("hidden");
     const noteCreated = {
         title: title.value,
         body: body.value
     };
     // Save JSON string to localStorage
     localStorage.setItem("noteCreated", JSON.stringify(noteCreated));
-    
+    parsedata();
     // Wipe inputs dry clean
     title.value = "";
     body.value = "";
 }
+}
 
-// 4. Event Listeners
-saveButton.addEventListener("click", () => {
-    saveNote();
-    
+function parsedata(){
     const checkSavedData = localStorage.getItem("noteCreated");
     if (checkSavedData) {
         const parsedData = JSON.parse(checkSavedData);
@@ -61,6 +39,13 @@ saveButton.addEventListener("click", () => {
         // Pass both the DOM target element and the data object into your module function
         ForListItem(listItems, parsedData);
     }
+
+}
+
+// 4. Event Listeners
+saveButton.addEventListener("click", () => {
+    saveNote();
+    
 });
 
 
